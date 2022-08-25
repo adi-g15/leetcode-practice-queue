b61c9e2a33d1b7d63076d1a9d1554c070b9e0d8f
Aditya Gupta <ag15035@gmail.com>
2022-08-25 09:08:57 +0530

    lib: Added get_queue, set_queue and file handling

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use std::{collections::VecDeque, io, path::PathBuf, fs};
+use xdg::BaseDirectories;
+
+fn get_store_filepath() -> PathBuf {
+    let xdg_dirs = BaseDirectories::with_prefix("lc-todo-queue").unwrap();
+
+    // SAFETY: If .unwrap() fails, we can assume something wrong with user's config files
+    //         and we can safely panic.
+    let data_dir = xdg_dirs.place_data_file("todo.txt").unwrap();
+
+    data_dir
+}
+
+
+fn get_queue() -> VecDeque<String> {
+    let mut queue = VecDeque::new();
+
+    let store_filepath = get_store_filepath();
+
+    let lines = fs::read_to_string(&store_filepath)
+        .expect(&format!("Unable to read {:?}!", &store_filepath));
+    
+    for line in lines.lines() {
+        queue.push_back(line.to_string());
+    }
+
+    queue
+}
+
+fn save_queue(queue: VecDeque<String>) -> Result<(),io::Error> {
+    let store_filepath = get_store_filepath();
+
+    let content = queue.iter().fold(String::new(), |acc, item| acc + "\n" + item);
+
+    fs::write(store_filepath, content)
+}
+
